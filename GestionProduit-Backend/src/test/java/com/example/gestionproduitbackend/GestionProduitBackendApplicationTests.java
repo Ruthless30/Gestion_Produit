1e6c2105f730f92db9ee36914b2fb545cebbf1d6
Msalem Amrou <dark.neutron1@gmail.com>
2025-12-07 20:13:12 +0100

    initial commit

---
+++ GestionProduit-Backend/src/test/java/com/example/gestionproduitbackend/GestionProduitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionproduitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
